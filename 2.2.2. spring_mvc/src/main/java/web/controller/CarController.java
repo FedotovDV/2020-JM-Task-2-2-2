c2d94829a7bb693785395395a011f3a0411a6023
Dmitriy <fed3375546@yandex.ru>
2020-07-30 01:04:12 +0300

    added localization

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -4,17 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class CarController {
 
     @GetMapping(value = "/cars")
-    public String carForm(ModelMap model) {
+    public String carForm(@RequestParam(defaultValue = "en") Locale locale, ModelMap model) {
         List<String> messages = new ArrayList<>();
-        model.addAttribute("title", "МАШИНЫ");
+        System.out.println(locale.toString());
+        if(locale.equals(Locale.forLanguageTag("ru"))){
+            model.addAttribute("title", "МАШИНЫ");
+        } else{
+            model.addAttribute("title", "CARS");
+        }
+
         messages.add("Car!");
         messages.add("I'm Spring MVC application");
         messages.add("5.2.7 version by jul'20 ");
